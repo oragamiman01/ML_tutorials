b947cd708edce140a51cb4bff92dd59f6946ae8e
Preston <oragamiman01@gmail.com>
2022-01-05 22:51:27 -0700

    working version

--- SVM_from_scratch.py
+++ SVM_from_scratch.py
@@ -26,7 +26,7 @@ class Support_Vector_Machine:
                     all_data.append(feature)
 
         self.max_feature_value = max(all_data)
-        self.min_featur_value = min(all_data)
+        self.min_feature_value = min(all_data)
         all_data = None
         
         #start with big steps, will progressively get smaller, costly performance to add more smaller steps
@@ -75,10 +75,41 @@ class Support_Vector_Machine:
     def predict(self, features): #use the sign of the equation below to determine if the data is -1 or +1 class
         #sign( x.w + b )
         classification = np.sign(np.dot(np.array(features), self.w) + self.b) #use np.dot for dot product, np.sign for sign of equation
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s = 200, marker="*", c=self.colors[classification])
 
         return classification
 
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        #hyperplane = x.w+b
+        def hyperplane(x,w,b,v):
+            return (-w[0] * x - b + v) / w[1]
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        #positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min,hyp_x_max], [psv1,psv2], 'k')
+
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min,hyp_x_max], [nsv1,nsv2], 'k')
+
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min,hyp_x_max], [db1,db2], 'y--')
+
+        plt.show()
 
 #create dictionary with -1 and +1 classes, each storing arrays of arrays
 data_dict = {-1:np.array([[1,7], [2,8], [3,8]]), 1:np.array([[5,1], [6,-1], [7,3]])} 
 
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+svm.visualize()
